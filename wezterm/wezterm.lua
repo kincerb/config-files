81d6fabeabd697f09c2f162e3ea29811a8c5b95f
kincerb <dev.bkincer@gmail.com>
2022-11-10 23:20:57 -0500

    Added a little more config.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,9 @@ local wezterm = require 'wezterm'
 return {
   font = wezterm.font 'FiraCode Nerd Font',
   font_size = 10.0,
-  color_scheme = 'Alabaster',
+  color_scheme = 'MaterialDark',
+  enable_scroll_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  tab_bar_at_bottom = true,
 }
 --# vim: ft=lua ts=2 sw=2 sts=2
