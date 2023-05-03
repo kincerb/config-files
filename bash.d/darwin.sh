a6ce42de200369133f0730ded52df2ba17487b0e
kincerb <kincerb@nationwide.com>
2023-05-03 16:56:00 -0400

    Simplified setup.

--- bash.d/darwin.sh
+++ bash.d/darwin.sh
@@ -1,17 +1,25 @@
-export EDITOR=/usr/local/bin/nvim
-export VISUAL="${VISUAL}"
-bash_helpers=("profile.d/bash_completion.sh" "bash_completion.d/ag.bashcomp.sh")
-bash_helpers+=("bash_completion.d/bash-builtins" "bash_completion.d/bat" "bash_completion.d/brew")
-bash_helpers+=("bash_completion.d/brew-services")
-bash_helpers+=("bash_completion.d/docker" "bash_completion.d/docker-compose" "bash_completion.d/fly")
-bash_helpers+=("bash_completion.d/git-completion.bash")
-bash_helpers+=("bash_completion.d/helm" "bash_completion.d/kubectl")
-bash_helpers+=("bash_completion.d/pipx")
-bash_helpers+=("bash_completion.d/tmux" "bash_completion.d/starship")
+export BREW_PREFIX=/opt/homebrew
+export BREW_BIN="${BREW_PREFIX}/bin"
+export BREW_ETC="${BREW_PREFIX}/etc"
+export BREW_BASH_COMPL_DIR="${BREW_PREFIX}/etc/bash_completion.d"
+export EDITOR="${BREW_BIN}/nvim"
+export VISUAL="${EDITOR}"
 
 launchctl setenv SSH_AUTH_SOCK "${SSH_AUTH_SOCK}"
 
-for bin_path in "/usr/local/sbin" "${HOME}/.krew/bin"; do
+for _helper in $(compgen -f "${HOME}/.local/share/bash_completion.d/"); do
+    if [ -e "${_helper}" ]; then
+        source "${_helper}"
+    fi
+done
+unset _helper
+
+bin_paths=("${BREW_BIN}" "${BREW_PREFIX}/sbin")
+bin_paths+=("${HOME}/Library/Python/3.10/bin")
+bin_paths+=("${HOME}/Library/Python/3.11/bin")
+bin_paths+=("${HOME}/.local/fzf/bin")
+
+for bin_path in  ${bin_paths[@]}; do
     if [ -d "${bin_path}" ]; then
         if ! [[ "${PATH}" =~ "${bin_path}" ]]; then
             export PATH="${bin_path}:$PATH"
@@ -19,22 +27,17 @@ for bin_path in "/usr/local/sbin" "${HOME}/.krew/bin"; do
     fi
 done
 unset bin_path
-
-alias chrome_canary='(/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --proxy-server=http://wireguard01.lan:3128 2>&1 &>/dev/null &)'
-alias xor_decode="python3 -c \"import base64; import sys; print(''.join(chr(ord(x) ^ ord('_')) for x in base64.b64decode(sys.argv[1].replace('{xor}', '')).decode()))\""
-alias awk=/usr/local/bin/gawk
-alias sed=/usr/local/bin/gsed
-
-for bash_helper in ${bash_helpers[@]}; do
-    if [ -e "/usr/local/etc/${bash_helper}" ] && [ -f "/usr/local/etc/${bash_helper}" ]; then
-        source "/usr/local/etc/${bash_helper}"
-    fi
-done
-unset bash_helper
+unset bin_paths
 
 if [ -n "${ITERM_SESSION_ID}" ] && [ -e "${HOME}/.iterm2_shell_integration.bash" ]; then
     source "${HOME}/.iterm2_shell_integration.bash"
 fi
+
+alias chrome_canary='(/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --proxy-server=http://wireguard01.lan:3128 2>&1 &>/dev/null &)'
+alias xor_decode="python3 -c \"import base64; import sys; print(''.join(chr(ord(x) ^ ord('_')) for x in base64.b64decode(sys.argv[1].replace('{xor}', '')).decode()))\""
+alias awk="${BREW_BIN}/awk"
+alias sed="${BREW_BIN}/gsed"
+alias pg_dump="${BREW_PREFIX}/Cellar/postgresql@12/12.14/bin/pg_dump"
 
 chrome_app() {
     if [ "${#}" -ne 1 ]; then
@@ -55,22 +58,22 @@ deploy() {
 }
 
 brew_cleanup() {
-    for x in /usr/local/Cellar/*; do
+    for x in /opt/homebrew/Cellar/*; do
         brew cleanup "${x##*/}"
     done
 }
 
 pycharm() {
     unset TMUX
-    open "/Users/kincerb/Applications/JetBrains Toolbox/PyCharm Professional.app"
+    open "${HOME}/Applications/JetBrains Toolbox/PyCharm Professional.app"
 }
 
 datagrip() {
     unset TMUX
-    open "/Users/kincerb/Applications/JetBrains Toolbox/DataGrip.app"
+    open "${HOME}/Applications/JetBrains Toolbox/DataGrip.app"
 }
 
 webstorm() {
     unset TMUX
-    open "/Users/kincerb/Applications/JetBrains Toolbox/WebStorm.app"
+    open "${HOME}/Applications/JetBrains Toolbox/WebStorm.app"
 }
