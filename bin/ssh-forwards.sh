98ed459fa4abf984bec89bc1e8e354edee8edb8c
kincerb <kincerb@nationwide.com>
2023-05-30 15:43:27 -0400

    Initial add.

---
+++ bin/ssh-forwards.sh
@@ -0,0 +1,27 @@
+#!/opt/homebrew/bin/bash
+
+FORWARDERS=("forward-to-vm" "forward-to-wireguard")
+
+
+stop() {
+    local _pids
+    local _pid
+    for _pid in $(pgrep -U ${UID} -f ssh-forwards.sh |xargs); do
+        _pids+=("${_pid}")
+    done
+}
+
+
+main() {
+    local _host
+    while true; do
+        for _host in ${FORWARDERS[@]}; do
+            if (ssh -O check "${_host}" 2>&1 |grep "No such" &>/dev/null); then
+                ssh -Nnf "${_host}"
+            fi
+        done
+        sleep 30
+    done
+}
+
+main
