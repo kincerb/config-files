31ffe5274f9a06cf273a23fb23c024ae3fc32c40
kincerb <kincerb@nationwide.com>
2023-08-08 14:43:23 -0400

    Added some FZF stuff

--- bash.d/common.sh
+++ bash.d/common.sh
@@ -64,7 +64,16 @@ export HISTSIZE=100000
 export HISTFILESIZE=100000
 export HISTIGNORE='history*'
 export HISTTIMEFORMAT='%F %T '
-export FZF_DEFAULT_OPTS='--color=dark'
+
+if (which fzf &>/dev/null); then
+    export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200' --select-1 --exit-0"
+    export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+    export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+--color=dark
+--color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe
+--color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef
+'
+fi
 
 # Source in all shared configs
 for x in ~/.config/bash.d/{colors,secrets,aliases,functions}.sh; do
