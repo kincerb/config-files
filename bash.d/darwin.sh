6ee0d55bf23d849185f64f42cf7e0e378c6cc441
kincerb <kincerb@nationwide.com>
2023-08-14 11:32:30 -0400

    Temp fix for fzf completion.

--- bash.d/darwin.sh
+++ bash.d/darwin.sh
@@ -20,6 +20,11 @@ done
 unset bin_path
 unset bin_paths
 
+
+if [ -n "${ITERM_SESSION_ID}" ] && [ -e "${HOME}/.iterm2_shell_integration.bash" ]; then
+    source "${HOME}/.iterm2_shell_integration.bash"
+fi
+
 for _helper in $(compgen -f "${HOME}/.local/share/bash_completion.d/"); do
     if [ -e "${_helper}" ]; then
         source "${_helper}"
@@ -27,16 +32,16 @@ for _helper in $(compgen -f "${HOME}/.local/share/bash_completion.d/"); do
 done
 unset _helper
 
-
-# if [ -n "${ITERM_SESSION_ID}" ] && [ -e "${HOME}/.iterm2_shell_integration.bash" ]; then
-#     source "${HOME}/.iterm2_shell_integration.bash"
-# fi
+for _helper in "${HOME}"/.local/fzf/shell/*.bash; do
+    source "${_helper}"
+done
+unset _helper
 
 alias chrome_canary='(/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --proxy-server=http://127.0.0.1:3128 &>/dev/null &)'
 alias xor_decode="python3 -c \"import base64; import sys; print(''.join(chr(ord(x) ^ ord('_')) for x in base64.b64decode(sys.argv[1].replace('{xor}', '')).decode()))\""
 alias awk="${BREW_BIN}/awk"
 alias sed="${BREW_BIN}/gsed"
-alias pg_dump="${BREW_PREFIX}/Cellar/postgresql@12/12.14/bin/pg_dump"
+alias pg_dump="${BREW_PREFIX}/Cellar/postgresql@12/12.15_2/bin/pg_dump"
 
 chrome_app() {
     if [ "${#}" -ne 1 ]; then
