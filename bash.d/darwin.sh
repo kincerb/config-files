7427d0b802269575afb5b30d103b3131435380cc
kincerb <kincerb@nationwide.com>
2023-08-15 10:27:59 -0400

    Updated some aliases.

--- bash.d/darwin.sh
+++ bash.d/darwin.sh
@@ -38,22 +38,22 @@ done
 unset _helper
 
 alias chrome_canary='(/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --proxy-server=http://127.0.0.1:3128 &>/dev/null &)'
+alias vivaldi='(/Applications/Vivaldi.app/Contents/MacOS/Vivaldi --proxy-server=http://127.0.0.1:3128 &>/dev/null &)'
 alias xor_decode="python3 -c \"import base64; import sys; print(''.join(chr(ord(x) ^ ord('_')) for x in base64.b64decode(sys.argv[1].replace('{xor}', '')).decode()))\""
 alias awk="${BREW_BIN}/awk"
 alias sed="${BREW_BIN}/gsed"
 alias pg_dump="${BREW_PREFIX}/Cellar/postgresql@12/12.15_2/bin/pg_dump"
 
-chrome_app() {
+web_app() {
     if [ "${#}" -ne 1 ]; then
-        echo -e "Usage:\n chrome_app [url]"
-        echo -e "\n  chrome_app https://netflix.com"
+        echo -e "Usage:\n web_app [url]"
+        echo -e "\n  web_app https://netflix.com"
         return
     fi
     local url="${1}"
-    (/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary \
-        --proxy-server=http://wireguard01.lan:3128 \
-        --app="${url}" \
-        --app-shell-user=dev.bkincer@gmail.com 2>&1 &>/dev/null &)
+    (/Applications/Vivaldi.app/Contents/MacOS/Vivaldi \
+        --proxy-server=http://127.0.0.1:3128 \
+        --app="${url}" 2>&1 &>/dev/null &)
 }
 
 deploy() {
