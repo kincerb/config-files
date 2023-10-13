d851552ccc2f97a0ef6b734fac97d54ebdda7a93
kincerb <kincerb@nationwide.com>
2023-10-13 16:04:26 -0400

    Fixed typo.

--- fish/conf.d/envvars.fish
+++ fish/conf.d/envvars.fish
@@ -3,7 +3,7 @@ set -gx VISUAL $EDITOR
 
 set -gx PAGER bat -p
 set -gx BAT_THEME gruvbox-dark
-set -gx SYSYEMD_PAGER bat --plain --language syslog
+set -gx SYSTEMD_PAGER bat --plain --language syslog
 set -gx SYSTEMD_PAGERSECURE true
 set -gx GOPATH ~/.local/go
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
