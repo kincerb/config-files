1463c69dd84498249efa3473b8b35f925ab3bfe0
kincerb <kincerb@nationwide.com>
2023-10-13 15:59:31 -0400

    Added vars for bat use with journalctl.

--- fish/conf.d/envvars.fish
+++ fish/conf.d/envvars.fish
@@ -1,9 +1,11 @@
 set -gx EDITOR nvim
 set -gx VISUAL $EDITOR
 
-set -gx PAGER "bat -p"
+set -gx PAGER bat -p
+set -gx BAT_THEME gruvbox-dark
+set -gx SYSYEMD_PAGER bat --plain --language syslog
+set -gx SYSTEMD_PAGERSECURE true
 set -gx GOPATH ~/.local/go
-set -gx BAT_THEME "gruvbox-dark"
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -gx MANROFFOPT "-c"
 set -gx ATUIN_NOBIND "true"
