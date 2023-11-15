3673365612fb13906e0d252216aecb64ddf2adb1
kincerb <kincerb@nationwide.com>
2023-11-15 13:11:14 -0500

    Added snippet keybindings.

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,7 +1,31 @@
 return {
   "L3MON4D3/LuaSnip",
   keys = function()
-    return {}
+    return {
+      {
+        "<c-j>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next"
+        end,
+        expr = true,
+        silent = true,
+        mode = "i",
+      },
+      {
+        "<c-j>",
+        function()
+          require("luasnip").jump(1)
+        end,
+        mode = "s",
+      },
+      {
+        "<c-k>",
+        function()
+          require("luasnip").jump(-1)
+        end,
+        mode = { "i", "s" },
+      },
+    }
   end,
   config = function()
     require("luasnip.loaders.from_snipmate").lazy_load()
