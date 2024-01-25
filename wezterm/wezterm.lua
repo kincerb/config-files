83a77d9e2332e18316f8004fb1a81a8cb368dd40
kincerb <dev.bkincer@gmail.com>
2024-01-25 00:24:53 -0500

    Updated keymaps.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,6 +8,7 @@ end
 
 config.font = wezterm.font("FantasqueSansM Nerd Font")
 config.font_size = 12.0
+config.bold_brightens_ansi_colors = true
 -- config.color_scheme = "Monokai Vivid"
 config.color_scheme = "Symphonic (Gogh)"
 -- config.color_scheme = "Atelierforest (dark) (terminal.sexy)"
@@ -26,13 +27,84 @@ config.inactive_pane_hsb = {
 	brightness = 0.7,
 }
 
-wezterm.on("update-right-status", function(window, pane)
+wezterm.on("update-status", function(window, pane)
 	local name = window:active_key_table()
 	if name then
 		name = "TABLE: " .. name
+	else
+		name = ""
 	end
-	window:set_right_status(name or "")
+
+	window:set_right_status(wezterm.format({ { Text = name } }))
 end)
+
+config.leader = { key = "Space", mods = "CTRL|SHIFT" }
+
+config.keys = {
+	{
+		key = "t",
+		mods = "CTRL|SHIFT",
+		action = act.SpawnTab({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "p",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({
+			name = "pane_control",
+			one_shot = false,
+		}),
+	},
+	{
+		key = "_",
+		mods = "CTRL|SHIFT",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "|",
+		mods = "CTRL|SHIFT",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Right"),
+	},
+}
+
+config.key_tables = {
+	pane_control = {
+		{ key = "-", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "\\", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "h", mods = "SHIFT", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "h", action = act.ActivatePaneDirection("Left") },
+		{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "l", mods = "SHIFT", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "l", action = act.ActivatePaneDirection("Right") },
+		{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "k", mods = "SHIFT", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "k", action = act.ActivatePaneDirection("Up") },
+		{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "j", mods = "SHIFT", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "j", action = act.ActivatePaneDirection("Down") },
+		{ key = "Escape", action = "PopKeyTable" },
+	},
+}
 
 return config
 --# vim: ft=lua ts=2 sw=2 sts=2
