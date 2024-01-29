cf458f450092416662e12225f9829fc4ca2261c7
kincerb <dev.bkincer@gmail.com>
2024-01-29 09:47:31 -0500

    Updated colorscheme.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,9 +9,7 @@ end
 config.font = wezterm.font("FantasqueSansM Nerd Font")
 config.font_size = 12.0
 config.bold_brightens_ansi_colors = true
--- config.color_scheme = "Monokai Vivid"
-config.color_scheme = "Symphonic (Gogh)"
--- config.color_scheme = "Atelierforest (dark) (terminal.sexy)"
+config.color_scheme = "Breeze (Gogh)"
 config.enable_scroll_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
