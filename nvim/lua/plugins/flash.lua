7b0361756535e1fac141afb5c5e85e99eab6e0ce
kincerb <kincerb@nationwide.com>
2024-02-22 22:28:13 -0500

    Added aerial.

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,16 +1,6 @@
 return {
   "folke/flash.nvim",
   keys = function()
-    return {
-      -- { "<c-s>", false, mode = { "c" } },
-      {
-        "s",
-        function()
-          require("flash").jump()
-        end,
-        mode = { "n" },
-        desc = "Flash",
-      },
-    }
+    return {}
   end,
 }
