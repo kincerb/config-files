3f669561a119ab828bcb943ae789d0b0f74e28b8
kincerb <kincerb@nationwide.com>
2024-02-29 13:51:51 -0500

    Increasing width.

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -2,10 +2,14 @@ return {
   "stevearc/aerial.nvim",
   opts = {
     layout = {
-      max_width = { 40, 0.4 },
+      max_width = 0.3,
+      width = 0.2,
+      min_width = 20,
       default_direction = "prefer_right",
       placement = "edge",
+      resize_to_content = true,
     },
+    close_automatic_events = { "unfocus", "unsupported" },
     on_attach = function(bufnr)
       vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
       vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
