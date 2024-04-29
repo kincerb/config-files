a43f07bd9e3f6d51a563dabdea4c631c0a4136c5
kincerb <kincerb@nationwide.com>
2024-04-29 15:53:50 -0400

    Fixed clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.python3_host_prog = "~/.local/venvs/neovim/bin/python"
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = "unnamed,unnamedplus"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.scrolloff = 999
