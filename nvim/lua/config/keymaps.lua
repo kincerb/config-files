865ac1d11868b81a253edf55e7b03ac8ed766cd2
kincerb <dev.bkincer@gmail.com>
2024-08-14 14:16:35 -0400

    Added diag nav with <CR>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,6 +3,18 @@
 -- Add any additional keymaps here
 local map = vim.keymap
 local opts = { noremap = true, silent = true }
+local diag_or_down = function()
+  if vim.diagnostic.is_enabled({ ns_id = nil, bufnr = 0 }) then
+    if #vim.diagnostic.count(0) ~= 0 then
+      vim.diagnostic.goto_next()
+      vim.api.nvim_feedkeys("zt", "n", false)
+    else
+      vim.api.nvim_feedkeys("j", "n", false)
+    end
+  else
+    vim.api.nvim_feedkeys("j", "n", false)
+  end
+end
 
 map.del("n", "<leader>ft")
 map.del("n", "<leader>fT")
@@ -16,6 +28,7 @@ map.set("i", "<c-CR>", function()
   local _keys = vim.api.nvim_replace_termcodes("<ESC>j", true, false, true)
   vim.api.nvim_feedkeys(_keys, "n", false)
 end, opts)
+map.set("n", "<CR>", diag_or_down, opts)
 map.set("i", "jk", "<ESC>", { noremap = true })
 map.set("n", "==", vim.diagnostic.open_float)
 map.set("n", "[d", vim.diagnostic.goto_prev, opts)
