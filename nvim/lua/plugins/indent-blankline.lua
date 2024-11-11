667f5b3ef589de1af38d937cd5d4bf9f7524db04
kincerb <dev.bkincer@gmail.com>
2024-11-11 12:37:23 -0500

    Tweaked colors

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,10 +1,10 @@
 local hooks = require("ibl.hooks")
 local highlight = {
-  "RainbowRed",
-  "RainbowYellow",
+  "RainbowGrey",
   "RainbowBlue",
-  "RainbowOrange",
+  "RainbowYellow",
   "RainbowGreen",
+  "RainbowOrange",
   "RainbowViolet",
   "RainbowCyan",
 }
@@ -13,22 +13,22 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     init = function()
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-        vim.api.nvim_set_hl(0, "ScopeBold", { bg = "NONE", fg = "NONE", bold = true })
+        vim.api.nvim_set_hl(0, "RainbowGrey", { fg = "#5A6D73" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#0E4573" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#6E6A01" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#367D04" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#874201" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#590273" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#066B78" })
+        vim.api.nvim_set_hl(0, "ScopeBold", { bg = "NONE", fg = "#80121B", bold = false })
       end)
     end,
     opts = {
       indent = { highlight = highlight },
       scope = {
-        enabled = false,
-        show_start = false,
-        show_end = false,
+        enabled = true,
+        show_start = true,
+        show_end = true,
         highlight = { "ScopeBold" },
       },
     },
