5a5f41f275abefcf9cc6c874b4bb98fbdc009f00
kincerb <dev.bkincer@gmail.com>
2024-11-11 16:11:57 -0500

    Modified colors

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,9 +1,9 @@
 local hooks = require("ibl.hooks")
 local highlight = {
   "RainbowGrey",
+  "RainbowRed",
   "RainbowBlue",
   "RainbowYellow",
-  "RainbowGreen",
   "RainbowOrange",
   "RainbowViolet",
   "RainbowCyan",
@@ -14,13 +14,13 @@ return {
     init = function()
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
         vim.api.nvim_set_hl(0, "RainbowGrey", { fg = "#5A6D73" })
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#80121B" })
         vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#0E4573" })
         vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#6E6A01" })
-        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#367D04" })
         vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#874201" })
         vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#590273" })
         vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#066B78" })
-        vim.api.nvim_set_hl(0, "ScopeBold", { bg = "NONE", fg = "#80121B", bold = false })
+        vim.api.nvim_set_hl(0, "ScopeBold", { bg = "NONE", fg = "#367D04", bold = true })
       end)
     end,
     opts = {
