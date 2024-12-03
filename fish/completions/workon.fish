b754fd0848926d181ae0df24cefb6d342dbbcb4b
kincerb <dev.bkincer@gmail.com>
2024-12-03 13:03:13 -0500

    Switched arg

--- fish/completions/workon.fish
+++ fish/completions/workon.fish
@@ -1,12 +1,12 @@
-set -l possible_envs (fd --type directory --unrestricted --max-depth 1 . "$HOME/.local/venvs")
+set --local possible_envs (fd --type directory --unrestricted --max-depth 1 . "$HOME/.local/venvs")
 
 for _dirname in (fd --type directory --unrestricted --exclude "site-packages" --exclude ".mypy_cache" --format "{//}" "^(venv|.git)\$" "$HOME/Projects")
     if not contains $_dirname $possible_envs
-        set -a possible_envs $_dirname
+        set --prepend possible_envs $_dirname
     end
 end
 
-complete -c workon -f
-complete -c workon -l jump
-complete -c workon -l activate
-complete -c workon -a (echo $possible_envs)
+complete --command workon --no-files
+complete --command workon --short-option n
+complete --command workon --long-option no-activate
+complete --command workon --arguments --keep-order (echo $possible_envs)
--- fish/functions/workon.fish
+++ fish/functions/workon.fish
@@ -1,26 +1,28 @@
 function workon --description "Start work on project"
-    argparse a/activate -- $argv
+    argparse n/no-activate -- $argv
 
     if test (count $argv) -eq 0
-        set -f project_dir $PWD
+        set --function project_dir $PWD
     else
-        set -f project_dir $argv[1]
+        set --function project_dir $argv[1]
     end
 
     cd $project_dir
 
-    if set -ql _flag_activate
-        if test -e $project_dir/bin/activate.fish
-            set -f venv_dir $project_dir
-        else if test -e $project_dir/venv/bin/activate.fish
-            set -f venv_dir $project_dir/venv
-        else if test -e (path dirname $project_dir)/venv/bin/activate.fish
-            set -f venv_dir (path dirname $project_dir)/venv
-        end
+    if set --query --local _flag_no-activate
+        return
+    end
 
-        if set --query venv_dir
-            source $venv_dir/bin/activate.fish
-        end
+    if test -e $project_dir/bin/activate.fish
+        set --function venv_dir $project_dir
+    else if test -e $project_dir/venv/bin/activate.fish
+        set --function venv_dir $project_dir/venv
+    else if test -e (path dirname $project_dir)/venv/bin/activate.fish
+        set --function venv_dir (path dirname $project_dir)/venv
+    end
+
+    if set --query venv_dir
+        source $venv_dir/bin/activate.fish
     end
 
     if test -e $project_dir/.env.fish
