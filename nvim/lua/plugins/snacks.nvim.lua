311c37f23dd66f4c7e181b2fb4f79d667cceda23
kincerb <dev.bkincer@gmail.com>
2025-01-02 17:53:22 -0500

    Removed force

--- nvim/lua/plugins/snacks.nvim.lua
+++ nvim/lua/plugins/snacks.nvim.lua
@@ -14,15 +14,15 @@ return {
     lazy = false,
     priority = 1000,
     init = function()
-      vim.api.nvim_set_hl(0, "Indent1", { fg = "#5A6D73", force = true })
-      vim.api.nvim_set_hl(0, "Indent2", { fg = "#f2960c", force = true })
-      vim.api.nvim_set_hl(0, "Indent3", { fg = "#0E4573", force = true })
-      vim.api.nvim_set_hl(0, "Indent4", { fg = "#b7d111", force = true })
-      vim.api.nvim_set_hl(0, "Indent5", { fg = "#d11711", force = true })
-      vim.api.nvim_set_hl(0, "Indent6", { fg = "#6E6A01", force = true })
-      vim.api.nvim_set_hl(0, "Indent7", { fg = "#590273", force = true })
-      vim.api.nvim_set_hl(0, "Indent8", { fg = "#066B78", force = true })
-      vim.api.nvim_set_hl(0, "IndentScope", { fg = "#05f731", force = true })
+      vim.api.nvim_set_hl(0, "Indent1", { fg = "#5A6D73" })
+      vim.api.nvim_set_hl(0, "Indent2", { fg = "#f2960c" })
+      vim.api.nvim_set_hl(0, "Indent3", { fg = "#0E4573" })
+      vim.api.nvim_set_hl(0, "Indent4", { fg = "#b7d111" })
+      vim.api.nvim_set_hl(0, "Indent5", { fg = "#d11711" })
+      vim.api.nvim_set_hl(0, "Indent6", { fg = "#6E6A01" })
+      vim.api.nvim_set_hl(0, "Indent7", { fg = "#590273" })
+      vim.api.nvim_set_hl(0, "Indent8", { fg = "#066B78" })
+      vim.api.nvim_set_hl(0, "IndentScope", { fg = "#05f731" })
     end,
     opts = {
       indent = {
