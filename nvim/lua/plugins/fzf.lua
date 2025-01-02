a2cd76b8716815b8e956934735e5b323530503e1
kincerb <dev.bkincer@gmail.com>
2025-01-02 17:53:38 -0500

    Added fzf-lua config

---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "echasnovski/mini.icons" },
+    opts = {
+      defaults = {
+        file_icons = "mini",
+      },
+      winopts = {
+        width = 0.8,
+        height = 0.5,
+      },
+    },
+  },
+}
