8e985bb8a61df2566e5e84100609463c6c7c4e99
kincerb <dev.bkincer@gmail.com>
2025-01-02 14:42:33 -0500

    Fix indent hl

--- nvim/lua/plugins/snacks.nvim.lua
+++ nvim/lua/plugins/snacks.nvim.lua
@@ -1,12 +1,12 @@
 local highlight = {
-  "SnacksIndent1",
-  "SnacksIndent2",
-  "SnacksIndent3",
-  "SnacksIndent4",
-  "SnacksIndent5",
-  "SnacksIndent6",
-  "SnacksIndent7",
-  "SnacksIndent8",
+  "Indent1",
+  "Indent2",
+  "Indent3",
+  "Indent4",
+  "Indent5",
+  "Indent6",
+  "Indent7",
+  "Indent8",
 }
 return {
   {
@@ -14,15 +14,15 @@ return {
     lazy = false,
     priority = 1000,
     init = function()
-      vim.api.nvim_set_hl(0, "SnacksIndent1", { fg = "#5A6D73" })
-      vim.api.nvim_set_hl(0, "SnacksIndent2", { fg = "#80121B" })
-      vim.api.nvim_set_hl(0, "SnacksIndent3", { fg = "#0E4573" })
-      vim.api.nvim_set_hl(0, "SnacksIndent4", { fg = "#039c0b" })
-      vim.api.nvim_set_hl(0, "SnacksIndent5", { fg = "#6E6A01" })
-      vim.api.nvim_set_hl(0, "SnacksIndent6", { fg = "#874201" })
-      vim.api.nvim_set_hl(0, "SnacksIndent7", { fg = "#590273" })
-      vim.api.nvim_set_hl(0, "SnacksIndent8", { fg = "#066B78" })
-      vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = "#039c0b" })
+      vim.api.nvim_set_hl(0, "Indent1", { fg = "#5A6D73", force = true })
+      vim.api.nvim_set_hl(0, "Indent2", { fg = "#f2960c", force = true })
+      vim.api.nvim_set_hl(0, "Indent3", { fg = "#0E4573", force = true })
+      vim.api.nvim_set_hl(0, "Indent4", { fg = "#b7d111", force = true })
+      vim.api.nvim_set_hl(0, "Indent5", { fg = "#d11711", force = true })
+      vim.api.nvim_set_hl(0, "Indent6", { fg = "#6E6A01", force = true })
+      vim.api.nvim_set_hl(0, "Indent7", { fg = "#590273", force = true })
+      vim.api.nvim_set_hl(0, "Indent8", { fg = "#066B78", force = true })
+      vim.api.nvim_set_hl(0, "IndentScope", { fg = "#05f731", force = true })
     end,
     opts = {
       indent = {
@@ -40,7 +40,7 @@ return {
           enabled = true,
           underline = true,
           only_current = false,
-          hl = "SnacksIndentScope",
+          hl = "IndentScope",
         },
       },
     },
