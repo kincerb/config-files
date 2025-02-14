c4c7406193d553bf7ea9d591cd978168f01d3f88
kincerb <kincerb@nationwide.com>
2025-02-14 12:08:02 -0500

    Updated blink keymaps

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -3,8 +3,41 @@ return {
     "saghen/blink.cmp",
     opts = {
       completion = {
+        trigger = { show_in_snippet = false },
+        list = { selection = { preselect = false, auto_insert = false } },
         accept = {
           auto_brackets = { enabled = true },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
+        ghost_text = { enabled = true, show_with_selection = true, show_without_selection = false },
+      },
+      keymap = {
+        preset = "super-tab",
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.snippet_forward()
+            else
+              return cmp.select_next()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.snippet_backward()
+            else
+              return cmp.select_prev()
+            end
+          end,
+          "snippet_backward",
+          "fallback",
         },
       },
     },
