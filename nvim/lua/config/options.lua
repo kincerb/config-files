74d1c40654e98a8da7836a5f165c5cd60b9aa9c1
kincerb <kincerb@nationwide.com>
2025-03-11 12:14:15 -0400

    Added picker option

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,10 +4,12 @@
 vim.g.python3_host_prog = "~/.local/venvs/neovim/bin/python"
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
+vim.g.lazyvim_picker = "fzf"
 vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamed,unnamedplus"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.scrolloff = 0 -- set to 999 for padding at bottom
+vim.opt.scrolloff = 999 -- set to 999 for padding at bottom
+vim.opt.sidescrolloff = 8
 -- disable automatic comment leader ofter hitting 'o' or 'O' see :help fo-table
 vim.opt.formatoptions:remove({ "o" })
 -- vim.opt.foldmethod = "expr"
