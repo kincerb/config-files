6f7ed746200578d71d9565e335e7aab7edff11ee
kincerb <kincerb@nationwide.com>
2025-03-12 18:42:57 -0400

    Added GPG_TTY

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 set fish_greeting
 
+set --global --export GPG_TTY (tty)
+
 if test \( -z "$SSH_CONNECTION" \) -o \( "$TERM_PROGRAM" = WezTerm \)
     set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
     if test ! -S "$SSH_AUTH_SOCK"
