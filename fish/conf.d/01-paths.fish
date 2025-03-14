fc13e97990a3c26d28e06c327dd7aaf8d1ee0013
kincerb <kincerb@nationwide.com>
2025-03-14 17:14:25 -0400

    Updated path

--- fish/conf.d/01-paths.fish
+++ fish/conf.d/01-paths.fish
@@ -1,11 +1,12 @@
 set --global --export GOPATH $HOME/.local/go
 
-set custom_paths $HOME/.local/bin $GOPATH/bin $HOME/.cargo/bin
-set man_paths /usr/share/man /usr/local/share/man /usr/local/man
+set homebrew /home/linuxbrew/.linuxbrew
+set custom_paths $homebrew/bin $HOME/.local/bin $GOPATH/bin $HOME/.cargo/bin
+set man_paths $homebrew/share/man /usr/share/man /usr/local/share/man /usr/local/man
 
 if not set -q VIRTUAL_ENV
     for _path in $custom_paths
-        fish_add_path --append --global $_path
+        fish_add_path --append --move --path --global $_path
     end
 end
 
@@ -14,13 +15,5 @@ for _path in $man_paths
         if not contains $_path $MANPATH
             set --global --export --append MANPATH $_path
         end
-    end
-end
-
-if test \( -e /home/linuxbrew/.linuxbrew/bin/brew \)
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    set --local brew_comp "$HOMEBREW_PREFIX"/share/fish/vendor_completions.d
-    if not contains $brew_comp $fish_complete_path
-        set --global --export --prepend fish_complete_path $brew_comp
     end
 end
