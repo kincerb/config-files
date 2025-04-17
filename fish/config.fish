fd7cd2262fb7b9c36c46681e63699d3d5e3a421a
kincerb <dev.bkincer@gmail.com>
2025-04-17 16:05:23 -0400

    Added source of secrets

--- fish/config.fish
+++ fish/config.fish
@@ -14,6 +14,10 @@ function fish_title
     pwd
 end
 
+if test -e "$HOME"/.config/fish/secrets.fish
+    source "$HOME"/.config/fish/secrets.fish
+end
+
 # Set settings for https://github.com/franciscolourenco/done
 set -U __done_min_cmd_duration 5000
 set -U __done_notification_urgency_level normal
