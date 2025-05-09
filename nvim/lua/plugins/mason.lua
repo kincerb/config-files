eb600fa10fe561d37233e36d4097ada9918179c3
kincerb <dev.bkincer@gmail.com>
2025-05-09 13:27:28 -0400

    Added pinned version until upstream is compatible again

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,5 +1,10 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
+  version = "1.11.0",
+  dependencies = {
+    "mason-org/mason-lspconfig.nvim",
+    version = "1.32.0",
+  },
   opts = {
     ensure_installed = {
       "debugpy",
