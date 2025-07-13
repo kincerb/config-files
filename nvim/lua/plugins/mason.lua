8571aa2ab502ca5562601705a1fc9b266b56eb2c
kincerb <dev.bkincer@gmail.com>
2025-07-13 16:39:12 -0400

    Removed pin

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,10 +1,5 @@
 return {
   "mason-org/mason.nvim",
-  version = "1.11.0",
-  dependencies = {
-    "mason-org/mason-lspconfig.nvim",
-    version = "1.32.0",
-  },
   opts = {
     ensure_installed = {
       "debugpy",
