9acca23d42577d69df30f58d454775ef464181d0
kincerb <kincerb@nationwide.com>
2025-07-18 11:02:19 -0400

    Added lowercase

--- fish/functions/proxy.fish
+++ fish/functions/proxy.fish
@@ -3,25 +3,30 @@ function proxy
     set -f ignore localhost,127.0.0.0/0,nwie.net,192.168.0.0/16,10.0.0.0/8
 
     switch $argv[1]
-    case squid home on
-        set -f target http://127.0.0.1:3128
-    case off
-        for prefix in $prefixes
-            set -e {$prefix}_PROXY
-        end
-        set -e NO_PROXY
-        return
-    case status
-        for prefix in $prefixes
-            echo \t{$prefix}_PROXY:\t(eval echo \${$prefix}_PROXY)
-        end
-        return
-    case '*'
-        set -f target $argv[1]
+        case squid home on
+            set -f target http://127.0.0.1:3128
+        case off
+            for prefix in $prefixes
+                set -e (string join '_' {$prefix} PROXY)
+                set -e (string join '_' (string lower {$prefix}) proxy)
+            end
+            set -e NO_PROXY
+            set -e no_proxy
+            return 0
+        case status
+            for prefix in $prefixes
+                echo \t{$prefix}_PROXY:\t(eval echo \${$prefix}_PROXY)
+                echo \t(string join '_' (string lower {$prefix}) proxy):\t(eval echo \$(string join '_' (string lower {$prefix}) proxy))
+            end
+            return 0
+        case '*'
+            set -f target $argv[1]
     end
 
     for prefix in $prefixes
-        set -gx {$prefix}_PROXY $target
+        set -gx (string join '_' {$prefix} PROXY) $target
+        set -gx (string join '_' (string lower {$prefix}) proxy) $target
     end
     set -gx NO_PROXY $ignore
+    set -gx no_proxy $ignore
 end
