b74a38df7e89229194340a6030f3a8591e2ad053
kincerb <dev.bkincer@gmail.com>
2025-08-28 12:20:19 -0400

    Update to use main branch

--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -1,6 +1,6 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  branch = "regexp",
+  branch = "main",
   dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
   ft = "python",
   event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
