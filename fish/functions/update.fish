49dfe866ccfe11801aa42dbd0152ef4c7f78f991
kincerb <dev.bkincer@gmail.com>
2025-09-29 17:21:49 -0400

    Updated to proper command usage

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,7 +1,7 @@
 function update --description "Run updates for apt, brew, flatpak"
     command sudo apt update && sudo apt -y upgrade
     command brew update && brew upgrade
-    command sudo flatpak --assumeyes update
+    command flatpak --system --assumeyes update
     command flatpak --user --assumeyes update
     command nvim --headless -c "Lazy! update" -c qall
 end
