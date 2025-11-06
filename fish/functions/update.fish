ec6fe2039d5d3a713f4259294398bc4cf356096c
kincerb <dev.bkincer@gmail.com>
2025-11-06 12:47:20 -0500

    Added some checks before running commands

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,7 +1,20 @@
 function update --description "Run updates for apt, brew, flatpak"
-    command sudo apt update && sudo apt -y upgrade
-    command brew update && brew upgrade
-    command sudo flatpak --system --assumeyes update
-    command flatpak --user --assumeyes update
-    command nvim --headless -c "Lazy! update" -c qall
+    if command -qs apt
+        command sudo apt update && sudo apt -y upgrade
+    else if command -qs garuda-update
+        command sudo garuda-update -noconfirm
+    end
+
+    if command -qs brew
+        command brew update && brew upgrade
+    end
+
+    if command -qs flatpak
+        command sudo flatpak --system --assumeyes update
+        command flatpak --user --assumeyes update
+    end
+
+    if command -qs nvim
+        command nvim --headless -c "Lazy! update" -c qall
+    end
 end
